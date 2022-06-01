fb06ce20a6e108ca8b218d2772dc2c941bfd6d83
Immertion <ezepiz@mail.ru>
2022-06-01 18:02:03 +1000

    fix code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,10 +23,10 @@ namespace Calc
         Operand operand1 = null;
         Operand operand2 = null;
         Base_operator op = null;
+   
         Operand memory_operand = new Operand(0);
         
         string buffer = "0";
-        string buf_up = "";
         string history = "";
 
 
@@ -37,64 +37,63 @@ namespace Calc
 
         private void key_use(object sender, KeyEventArgs e)
         {
-
             if (e.Key == Key.D0)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '0';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D1)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '1';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D2)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '2';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D3)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '3';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D4)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '4';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D5)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '5';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D6)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '6';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D7)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '7';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D8)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '8';
                 Text.Content = buffer;
             }
             if (e.Key == Key.D9)
             {
-                check_zero();
+                remove_leading_zero();
                 buffer += '9';
                 Text.Content = buffer;
             }
@@ -103,18 +102,16 @@ namespace Calc
             if (e.Key == Key.Back) delete_last_click(sender, e);
             if (e.Key == Key.OemPipe) division_click(sender, e);
             if (e.Key == Key.Multiply) myltiply_click(sender, e);
-            if (e.Key == Key.OemPeriod) Add_digit(sender, e);
+            if (e.Key == Key.OemPeriod) add_digit(sender, e);
             if (e.Key == Key.Enter) result_click(sender, e);
         }
-        private void Add_digit(object sender, RoutedEventArgs e)
+        private void add_digit(object sender, RoutedEventArgs e)
         {
-            check_zero();
-            var button = sender as Button;
-            var atr = button.Tag;
-            buffer += atr;
+            remove_leading_zero();
+            buffer += (sender as Button).Tag;
             Text.Content = buffer;
         }
-        public void check_zero()
+        public void remove_leading_zero()
         {
             if (buffer == "0")
             {
@@ -132,6 +129,21 @@ namespace Calc
                 operand1 = new Operand(Convert.ToDouble(buffer));
             }
         }
+        public void Calc()
+        {
+            behind_text.Content += buffer;
+            if (op != null)
+            {
+                operand1 = op.Calculate(operand1, operand2);
+                operand2.value = operand1.value;
+                buffer = "0";
+                Text.Content = operand1.value.ToString();
+            }
+            else
+            {
+                buffer = "0";
+            }
+        }
         public void print_operand()
         {
             if (op != null)
@@ -147,119 +159,61 @@ namespace Calc
         }
         private void sum_click(object sender, RoutedEventArgs e)
         {
-            Plus plus = new Plus();
             sumarize();
-            buf_up += buffer;
-            if (op != null)
-            {
-                operand1 = op.Calculate(operand1, operand2);
-                operand2.value = 0;
-                buffer = "0";
-                Text.Content = operand1.value.ToString();
-            }
-            else
-            {
-                buffer = "0";
-                Text.Content = buffer;
-            }
-            op = plus;
-            buf_up += '+';
-            behind_text.Content = buf_up;
+            Calc();
+            op = new Plus();
+            behind_text.Content += "+";
         }
 
         private void division_click(object sender, RoutedEventArgs e)
         {
-            Division division = new Division();
             sumarize();
-            buf_up += buffer;
-            if (op != null)
-            {
-                operand1 = op.Calculate(operand1, operand2);
-                operand2.value = 0;
-                buffer = "0";
-                Text.Content = operand1.value.ToString();
-            }
-            else
-            {
-                buffer = "0";
-                Text.Content = buffer;
-            }
-            op = division;
-            buf_up += '/';
-            behind_text.Content = buf_up;
-
+            Calc();
+            op = new Division();
+            behind_text.Content += "/";
         }
 
         private void minus_click(object sender, RoutedEventArgs e)
-        {
-            Minus minus = new Minus();
+        { 
             sumarize();
-            buf_up += buffer;
-            if (op != null)
-            {
-                operand1 = op.Calculate(operand1, operand2);
-                operand2.value = 0;
-                buffer = "0";
-                Text.Content = operand1.value.ToString();
-            }
-            else
-            {
-                buffer = "0";
-                Text.Content = buffer;
-            }
-            op = minus;
-            buf_up += '-';
-            behind_text.Content = buf_up;
-
-
+            Calc();
+            op = new Minus();
+            behind_text.Content += "-";
         }
         private void myltiply_click(object sender, RoutedEventArgs e)
         {
-            Myltiply myltiply = new Myltiply();
             sumarize();
-            buf_up += buffer;
+            Calc();
+            op = new Myltiply();
+            behind_text.Content += "*";
+        }
+        public void calc_single(Additional_operator single_op)
+        {
             if (op != null)
             {
-                operand1 = op.Calculate(operand1, operand2);
-                operand2.value = 0;
-                buffer = "0";
-                Text.Content = operand1.value.ToString();
+                operand2 = single_op.Calculate(operand2);
             }
             else
             {
-                buffer = "0";
-                Text.Content = buffer;
+                operand1 = single_op.Calculate(operand1);
             }
-            op = myltiply;
-            buf_up += '*';
-            behind_text.Content = buf_up;
         }
         private void change_sign(object sender, RoutedEventArgs e)
         {
+            if (buffer == "0")
+            {
+                return;
+            }
             Sign sign = new Sign();
             sumarize();
-            if (op != null)
-            {
-                operand2 = sign.Calculate(operand2);
-            }
-            else
-            {
-                operand1 = sign.Calculate(operand1);
-            }
+            calc_single(sign);
             print_operand();
         }
         private void percent_click(object sender, RoutedEventArgs e)
         {
             Percent percent = new Percent();
             sumarize();
-            if (op != null)
-            {
-                operand2 = percent.Calculate(operand2);
-            }
-            else
-            {
-                operand1 = percent.Calculate(operand1);
-            }
+            calc_single(percent);
             print_operand();
         }
 
@@ -267,14 +221,7 @@ namespace Calc
         {
             Reverse reverse = new Reverse();
             sumarize();
-            if (op != null)
-            {
-                operand2 = reverse.Calculate(operand2);
-            }
-            else
-            {
-                operand1 = reverse.Calculate(operand1);
-            }
+            calc_single(reverse);
             print_operand();
         }
 
@@ -282,28 +229,14 @@ namespace Calc
         {
             Sqrt sqrt = new Sqrt();
             sumarize();
-            if (op != null)
-            {
-                operand2 = sqrt.Calculate(operand2);
-            }
-            else
-            {
-                operand1 = sqrt.Calculate(operand1);
-            }
+            calc_single(sqrt);
             print_operand();
         }
         private void square_click(object sender, RoutedEventArgs e)
         {
             Square square = new Square();
             sumarize();
-            if (op != null)
-            {
-                operand2 = square.Calculate(operand2);
-            }
-            else
-            {
-                operand1 = square.Calculate(operand1);
-            }
+            calc_single(square);
             print_operand();
         }
         private void delete_last_click(object sender, RoutedEventArgs e)
@@ -311,20 +244,18 @@ namespace Calc
             if (buffer.Length > 1)
             {
                 buffer = buffer.Remove(buffer.Length - 1);
-                Text.Content = buffer;
             }
             else
             {
                 buffer = "0";
-                Text.Content = buffer;
             }
+            Text.Content = buffer;
         }
         private void c_click(object sender, RoutedEventArgs e)
         {
             buffer = "0";
-            buf_up = "";
             op = null;
-            behind_text.Content = buf_up;
+            behind_text.Content = "";
             Text.Content = buffer;
         }
         private void ce_click(object sender, RoutedEventArgs e)
@@ -360,18 +291,15 @@ namespace Calc
         private void result_click(object sender, RoutedEventArgs e)
         {
             sumarize();
-            history = buf_up + buffer + " = ";
+            history = behind_text.Content + buffer + " = ";
             operand1 = op.Calculate(operand1, operand2);
             buffer = operand1.value.ToString();
             history += buffer;
             his.Items.Add(history);
             op = null;
-            buf_up = "";
             history = "";
-            behind_text.Content = buf_up;
+            behind_text.Content = "";
             Text.Content = buffer;
         }
-
-
     }
 }
